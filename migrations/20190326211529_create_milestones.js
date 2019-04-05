7b76c368503fdf1ee571de52eeada3a956417119
pmckeegan <pmckeegan@gmail.com>
2019-04-05 12:42:46 +0000

    milestones knex migrations exercise

--- migrations/20190326211529_create_milestones.js
+++ migrations/20190326211529_create_milestones.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {  
     return Promise.all([
       knex.schema.createTable('milestones', function(table){
-        table.increments('milestones_id');
+        table.increments('milestones_id').primary();
         table.string('description');
         table.date('date_achieved');
       })
